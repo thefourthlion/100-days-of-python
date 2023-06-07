0a1162594dac6bcc3b94c03bcfdb14e160b88846
TheFourthLion <the00110100lion@gmail.com>
2023-06-06 21:08:17 -0700

    add log non zip

---
+++ log_non_sip.py
@@ -0,0 +1,12 @@
+import os
+
+def check_folder(folder_path):
+    for root, dirs, files in os.walk(folder_path):
+        for file in files:
+            if not file.endswith('.zip'):
+                print(file)
+
+# Replace 'folder_path' with the actual path of the folder you want to check
+folder_path = 'D:\programming\'
+check_folder(folder_path)
+
